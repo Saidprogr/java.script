871bec1bc7f8a2e0a5fe6ef738d8300c884ea120
Saidprogr <sverqazov@gmail.com>
2025-12-11 17:49:05 +0400

    rubl-azn

--- app.js
+++ app.js
@@ -1,14 +1,91 @@
-const yas = prompt("yaşinizi yazin");
+// function fillBalance(15) {
+//   balance += n15;
+//   console.log('The balance has been increased');
+//   console.log(`Available: ${balans}`);
+// }
 
-if (yas !== null) {
-    const yasi = Number(yas);
+// let balance = 0;
 
-    if (!isNaN(yasi)) {
-        if (yasi < 21) {
-            alert("sayta giris qadagandir");
-            alert("düzgün yaş daxil edin.");
-        } else {
-            alert("İcazə verildi");
-        }
-    }
-}
+// fillBalance(5);
+
+
+
+
+// function logBalanceChange() {
+//   console.log('Balance changed');
+//   console.log(`Available: ${balans}`);
+// }
+// let balance = 0;
+
+
+
+
+// balance += 10;
+// logBalanceChange();
+
+
+
+
+
+
+
+
+
+
+// let balance = 0;
+// balance += 10;
+// console.log('Balance changed');
+// console.log(`Available: ${balance}');
+
+
+// balance -= 5;
+// console.log('Balance changed');
+// console.log(`Available: ${balance}');
+
+
+// balance *= 2;
+// balance += 5;
+// console.log('Balance changed');
+// console.log(`Available: ${balance}`);
+
+
+// balance -= 8;
+// console.log('Balance changed');
+// console.log(`Available: ${balance}');
+// // let balance = 0;
+// // balance += 10;
+// // console.log('Balance changed');
+// // console.log(`Available: ${balance}');
+  
+
+
+
+
+
+
+// balance -= 5;
+// console.log('Balance changed');
+// console.log(`Available: ${balans}');
+
+// balance *= 2;
+// balance += 5;
+// console.log('Balance changed');
+// console.log(`Available: ${balans}');
+
+// balance -= 8;
+// console.log('Balance changed');
+// console.log(`Available: ${balance}');
+
+
+let azn = Number(prompt("necese azn yaz"));
+
+let usdT= 1.7;
+let rubL = 0.0218;
+
+let usd = azn / usdT;
+let rub = azn / rubL;
+
+console.log("AZN:", azn);
+console.log("USD:", usd.toFixed(2));
+console.log("RUB:", rub.toFixed(2));
+ 
